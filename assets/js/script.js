e5c7ab7e5a0f2a2090e8d83c1a6e9eebae8ef6c2
Marissa Fandel <mfandel@alumni.nd.edu>
2022-02-25 23:16:59 -0700

    added function to change colors based on past present or future, fixed issue recalling schedule from localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,19 +2,19 @@
 // Display timeblocks for business hours ✅ 
 // Display save buttons next to each timeblock ✅ 
 
-// Colors indicate whether block of time is current, in the past, or in the future
+// Colors indicate whether block of time is current, in the past, or in the future ✅
 
 // Timeblocks have text area where user can input schedule details ✅
 
 // When save buttons are clicked, schedule details are saved to localStorage ✅
 
-// When user refreshes page, the text in the text areas is still displayed
+// When user refreshes page, the text in the text areas is still displayed ✅
 
 
 // Declare variables
 var today = $("#currentDay");
 var schedule = $(".container");
-var timeBLock = $(".time-block");
+var timeBlock = $(".time-block");
 var hour = $(".hour");
 var scheduleText = $("textarea");
 var saveButtons = $(".saveBtn");
@@ -23,72 +23,43 @@ var saveButtons = $(".saveBtn");
 // Display current day at top of page
 today.text("Current Workday: " + moment().format("dddd, MMMM Do YYYY"));
 
-function timeBlockColors () {
-    var currentHour = moment().hour();
-    console.log(currentHour);
-    // console.log(typeof(currentHour));
+// Function to pull from localStorage to display current schedule at reload of page
+function displaySaved() {
+    for (var i = 8; i <= 17; i++){
+        var savedSchedule = JSON.parse(localStorage.getItem(i));
+        // console.log(savedSchedule);
 
-    for (var i=0; i<scheduleText.length; i++) {
-        var scheduleTextHour = parseInt(scheduleText[i].name)
-        console.log(scheduleTextHour);
-        // console.log(typeof(scheduleTextHour));
-
-        if (scheduleTextHour == currentHour) {
-            hour.addClass("present");
-            scheduleText.addClass("present");
-        } else if (scheduleTextHour < currentHour) {
-            hour.addClass("past");
-            scheduleText.addClass("past");
-        } else {
-            hour.addClass("future");
-            scheduleText.addClass("future");
-        };
-    };
+        scheduleText.eq(i-8).text(savedSchedule);
+    }
 };
-// Call function to set schedule hour colors
-timeBlockColors();
+// Call Function to display previous schedule
+displaySaved();
 
+// Event Listener to save schedule data in localStorage
 saveButtons.on("click", saveData);
-
 function saveData(event) {
-    var hourKey = event.target.parentNode.children[0].textContent;
-    console.log(hourKey);
+    var hourKey = event.target.parentNode.children[1].name;
+    // console.log(hourKey);
     var userInput = event.target.parentNode.children[1].value;
     localStorage.setItem(hourKey, JSON.stringify(userInput));
 };
 
-
-
-// Add event listener to save user inputted data to localStorage
-// saveButtons.on("click",saveData);  // WHY DOES ONLY FIRST SAVE BUTTON WORK?
-// function saveData() {
-//     
-//     var hourKey = hour.text();  // WHY DOES HOUR NOT SHOW UP FOR KEY IN LOCALSTORAGE?
-
-//     
-// };
-
-
 // Function to keep inputted data on screen when user refreshes page
-function displayUserInput() {
-    // Pull data from localStorage and display on page
-    var savedUserInput = [
-        localStorage.getItem("8AM"),
-        localStorage.getItem("9AM"),
-        localStorage.getItem("10AM"),
-        localStorage.getItem("11AM"),
-        localStorage.getItem("12PM"),
-        localStorage.getItem("1PM"),
-        localStorage.getItem("2PM"),
-        localStorage.getItem("3PM"),
-        localStorage.getItem("4PM"),
-        localStorage.getItem("5PM"),
-    ];
-    console.log(savedUserInput);
+scheduleText.each (function () {
+    var scheduleTextHour = parseInt($(this).attr("name"));
+    // console.log(scheduleTextHour);
+    var currentHour = moment().hour();
+    // console.log(currentHour);
 
-    for (var i=0; i<savedUserInput.length; i++) {
-       scheduleText.text(savedUserInput[i])
+    if (scheduleTextHour === currentHour) {
+        $(this).siblings().addClass("present");
+        $(this).addClass("present");
+    } else if (scheduleTextHour < currentHour) {
+        $(this).siblings().addClass("past");
+        $(this).addClass("past");
+    } else {
+        $(this).siblings().addClass("future");
+        $(this).addClass("future");
     };
-};
-// Call function to keep user inputted data on screen
-displayUserInput();
+
+});
